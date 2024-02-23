286cddf74b959ba75029c23929f852610f9237e9
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2024-02-23 14:41:58 +0100

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { UserTypesModule } from './user-types/user-types.module';
+import { CountriesModule } from './countries/countries.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderStatusesModule } from './order-statuses/order-statuses.module';
+import { OrderDetailsModule } from './order-details/order-details.module';
+import { PromotionsModule } from './promotions/promotions.module';
+import { EventsModule } from './events/events.module';
+import { PaymentTypesModule } from './payment-types/payment-types.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
@@ -11,6 +22,18 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     DatabaseModule,
+    UsersModule,
+    UserTypesModule,
+    CountriesModule,
+    ProductsModule,
+    OrdersModule,
+    OrderStatusesModule,
+    OrderDetailsModule,
+    PromotionsModule,
+    EventsModule,
+    PaymentTypesModule,
+    FeedbackModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
